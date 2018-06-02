db5b373b86bc84aa8852c0f25bd91c14d70be9a7
Daniel Wise <danielwise19918@gmail.com>
2018-06-01 23:20:10 -0400

    side bar filtering

--- src/SearchArea.js
+++ src/SearchArea.js
@@ -1,13 +1,32 @@
 import React, {Component} from 'react'
 import './SearchArea.css'
+import escapeRegExp from 'escape-string-regexp'
+import sortBy from 'sort-by'
 
 class SearchArea extends Component{
 	state = {
 		query: ''
 	}
 
+	updateQuery = (query) =>{
+		this.setState({query: query.trim()})
+	}
+
+
+
 	render(){
+		const {query} = this.state
 		const locationNames = this.props.markers
+
+		let showingResults 
+		if(query){
+			const match = new RegExp(escapeRegExp(query), 'i')
+			showingResults = locationNames.filter((location) => match.test(location.name))
+		} else {
+			showingResults = locationNames
+		}
+
+		showingResults.sort(sortBy('name'))
 
 		return(
 		<div>
@@ -16,11 +35,12 @@ class SearchArea extends Component{
 	        type="text"
 	        placeholder="Search Locations"
 	        className="searchBar"
+	        onChange={(event) => this.updateQuery(event.target.value)}
            />
           </div>
            <div>
             <ul className="address-listings">
-            {locationNames.map((location, index) => (
+            {showingResults.map((location, index) => (
             		<li>{location.name}</li>
             	))}
 				</ul>
